6a19cd6eb69a43be321fef65e8a7dc3bf7c866a6
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-04-10 21:55:50 +0530

    java

---
+++ basic.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class largest {
+
+    public static void main(String[] args) {
+        int arr[]=new int[10];
+        arr[0]=10;
+        arr[2]=20;
+        arr[3]=20;
+        arr[4]=20;
+        arr[5]=20;
+        arr[6]=20;
+        arr[7]=20;
+        arr[8]=20;
+        arr[9]=20;
+
+        System.out.println(arr[5]);
+
+        String[] dd=new String[5];
+        dd[0]="dharshu";
+        dd[4]="kowshik";
+
+        System.out.println(dd[0]+ "" +dd[4]);
+
+
+        //length
+
+        String name[]=new String[2];
+        name[0]="keerthi";
+        name[1]="bhuvana";
+
+        System.out.println(name.length);
+
+
+
+
+
+    }
+}
