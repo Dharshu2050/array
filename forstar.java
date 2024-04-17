5e23b347024c908c189c028ca1515cb884a3cc12
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-04-17 22:35:00 +0530

    java

---
+++ forstar.java
@@ -0,0 +1,7 @@
+public class forstar {
+    public static void main(String[] args) {
+        for (int i = 1; i < 500; i++) {
+            System.out.println("*");
+        }
+    }
+}
--- occrrences.java
+++ occrrences.java
@@ -1,5 +1,5 @@
 public class occrrences {
-    public static void main(String[] args) {
+   /*  public static void main(String[] args) {
         int a[]={1,4,8,4,1,6,7};
         for (int i = 0; i < a.length; i++) {
             int temp=0;
@@ -11,5 +11,16 @@ public class occrrences {
            }
            count++;
         }
-    }
+    
+}*/
+       public static void main(String[] args) 
+     {
+         int[] n={1,3,1,4,6,6,3};
+         int searchnumber=3;
+
+     }
+    public static int occrrences (int[] n,int searchnumber) {
+        
+
+}
 }
