08cf712404ae78d86e95aa46dd79fe3f66483696
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-04-18 11:40:04 +0530

    java

---
+++ starpattern.java
@@ -0,0 +1,10 @@
+public class starpattern {
+    public static void main(String[] args) {
+        //for loop -loop aagurathugaka ,i 1,i<=5,i++,sop - *
+
+        for (int i = 0; i<=5; i++) {
+            System.out.println(" "+ " * " );
+         
+        }
+    }
+}
